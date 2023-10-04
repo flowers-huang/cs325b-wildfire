c9a6d1855b7495b054e4af557b1e5dc5049f70e4
topcat <9004403+ivanhigueram@users.noreply.github.com>
2023-10-04 12:01:28 -0700

    Update README for data provenance and add LS code
     - Update README to introduce the data a little bit.
     - Add Landsat class to retrieve imagery via the MS Planetary Computer
     - Add some main scripts to get the data
    
    Caveats and bugs:
     - This class can be improved for multiple query using GeoParquet. I
       have been a bit lazy to do it. Currently, you have to run this
       several times to get all the query results.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# cs325b-wildfire
-Github Repository for all code associated with the CS325B 2023 Wildfire group
+# Segmenting Wildfire Burned Area in the :earth_africa:
+
+## Tentative data sources
+
+Data in our GCP bucket has cropped [Landsat Collection 2][1] imagery for all
+available fires in California. Each data file has pre-fire and post-fire imagery
+captured during the fire season defined for the State (between day 152 and 273
+of the year). Since Landsat has an average revisit day between 8 and 16 days,
+many of the fires have several images. Notice that we have visible bands and
+infrared bands to calculate different vegetation and burning indexed, this might
+be relevant if you want to use this data for pre-training a model from scratch,
+but feel free to ignore it if you prefer fine-tune something. Native resolution
+for Landsat is 30 meters, which works pretty well for some of the purposes
+ecologists are interested in, but not for segmentation (a lot of the papers use
+Sentinel-2 data, 3 times higher resolution, for example), we should test this.
+
+Data is in NetCDF format and notice that before using you should use the quality 
+assurance data to clean some pixels that aren't informative (i.e. snow or clouds).
+If you feel you need more data, which I assume you will because is good for out-of
+distribution (OOD) issues, you can use the code I added under `src/landsat.py`. 
+Remember California vegetation is pretty particular, and the Mediterranean weather
+has climatic conditions that make the vegetation greener and is dominated by
+conifers, so is not exactly the tropical Amazon. 
+
+
+
+[1]: https://planetarycomputer.microsoft.com/dataset/landsat-c2-l2#Example-Notebook
---
+++ main/landsat_download.py
@@ -0,0 +1,58 @@
+import os
+import argparse
+import geopandas as gpd
+
+from src.landsat_get import Landsat
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path_to_shape", type=str, help="Path to extration shape")
+    parser.add_argument("--path_to_save", type=str, help="Path to save data")
+    parser.add_argument(
+        "--cloud_coverage", type=int, default=40, help="Cloud coverage treshold"
+    )
+    parser.add_argument(
+        "--buffer", type=int, default=180, help="Cloud coverage treshold"
+    )
+    parser.add_argument(
+        "--post",
+        type=bool,
+        action=argparse.BooleanOptionalAction,
+        help="Get post-fire data",
+    )
+
+    args = parser.parse_args()
+
+    # Load geometry and subset to wildfires only
+    mtbs = gpd.read_file(args.path_to_shape)
+    wildfires = mtbs[
+        (mtbs.Event_ID.str.contains("CA")) & (mtbs.Incid_Type == "Wildfire")
+    ]
+
+    # Create buffers for extraction! remember we need to calculate and offset for each
+    # fire
+    wildfires_meters = wildfires.to_crs(3310)  # NAD83 for CA
+
+    wildfire_buffers = wildfires.copy()
+    wildfire_buffers["geometry"] = wildfires_meters.buffer(args.buffer).to_crs(
+        4326
+    )  # Go back to mercator
+
+    print("Start class and start extraction")
+    # Start class and start retrieving files to path
+
+    if args.post:
+        type_extract = "post_year"
+    else:
+        type_extract = "pre_year"
+
+    ls = Landsat(
+        aoi_path=wildfire_buffers,
+        cloud_coverage=args.cloud_coverage,
+        save_path=args.path_to_save,
+        date_window=tuple([152, 273]),  # CA fire season period
+        date_window_type=type_extract,
+    )
+
+    print("Start downloading process -- tqdm.tqdm should start soon")
+    ls.execute_search_aoi()
